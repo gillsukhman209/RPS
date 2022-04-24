94f2580ce30ebc7181f41d40c866866393ba3770
gillsukhman209 <gillsukhman209@gmail.com>
2022-04-24 16:09:39 -0700

    added the functionality but design still very bad, about to push to main branch

--- backend.js
+++ backend.js
@@ -1,44 +1,142 @@
-options = ["Rock", "Paper", "Scissors"]
+const container = document.querySelector(".container")
+const buttons = document.querySelectorAll("button")
+
+let score = 0;
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+
+        let playerSelection;
+        switch (e.target.id) {
+            case "1":
+                playerSelection = "Rock";
+                break;
+            case "2":
+                playerSelection = "Paper";
+                break;
+
+            case "3":
+                playerSelection = "Scissors";
+                break;
+
+        }
+        let options = ["Rock", "Paper", "Scissors"]
 
 
-function computerPlay() {
-    rm = parseInt(Math.floor(Math.random() * 3))
-    return options[rm]
-}
-playerSelection = parseInt(prompt("whats your choice: (1) Rock, (2) Paper, (3) Scissors"))
+        function computerPlay() {
+            rm = parseInt(Math.floor(Math.random() * 3))
+            return options[rm]
+        }
+        start(playerSelection, computerPlay())
+
+        document.querySelector(".playerSelection").textContent = "You Picked: " + playerSelection;
+
+    })
+})
+
 
 function start(playerSelection, computerSelection) {
 
-    console.log("Computer selection is " + computerSelection)
+    document.querySelector(".computerSelection").textContent = "Computer Picked: " + computerSelection
+
     if (computerSelection == "Rock") {
 
-        if (playerSelection == 3) {
-            console.log("You lost")
-        } else if (playerSelection == 1) {
-            console.log("Tie")
-        } else if (playerSelection == 2) {
-            console.log("You won")
+        if (playerSelection == "Scissors") {
+            result("L")
+        } else if (playerSelection == "Rock") {
+            result("T")
+
+        } else if (playerSelection == "Paper") {
+            result("W")
+
+
         }
 
-    } else if (computerSelection == "Scissors") {
-        if (playerSelection == 2) {
-            console.log("You lost")
-
-        } else if (playerSelection == 3) {
-            console.log("Tie")
-
-        } else if (playerSelection == 1) {
-            console.log("You won")
-        }
-    } else if (computerSelection == "Paper") {
-        if (playerSelection == 1) {
-            console.log("You Lost")
-        } else if (playerSelection == 2) {
-            console.log("Tie")
-        } else if (playerSelection == 3) {
-            console.log("You won")
-        }
     }
+    switch (playerSelection) {
+        case "Rock":
+            switch (computerSelection) {
+                case "Rock":
+                    result("T")
+                    break;
+
+                case "Paper":
+                    result("L")
+                    break;
+
+                case "Scissors":
+                    result("W")
+                    break;
+
+            }
+            break;
+
+        case "Paper":
+            switch (computerSelection) {
+                case "Rock":
+                    result("W")
+                    break;
+
+                case "Paper":
+                    result("T")
+                    break;
+
+                case "Scissors":
+                    result("L")
+                    break;
+
+            }
+            break;
+
+
+        case "Scissors":
+            switch (computerSelection) {
+                case "Rock":
+                    result("L")
+                    break;
+
+                case "Paper":
+                    result("W")
+                    break;
+
+                case "Scissors":
+                    result("T")
+                    break;
+
+            }
+            break;
+
+    }
+
 }
 
-start(playerSelection, computerPlay())
+
+function result(output) {
+
+    switch (output) {
+        case "W":
+
+            container.style.cssText = "background: green;"
+
+            score += 1;
+            break;
+        case "L":
+
+            if (!score == 0) {
+                score -= 1;
+
+            }
+            container.style.cssText = "background: red;"
+
+
+            break;
+
+        case "T":
+            container.style.cssText = "background: gray;"
+            break;
+
+
+    }
+
+
+    document.querySelector(".scoreCounter").textContent = "Score is " + String(score)
+}
